749c6376e97f9e9d3946438fb62a7b047c666e08
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-09-24 13:18:49 +0300

    adding category component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Category from './components/category/Category';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Category />
     </div>
   );
 }
